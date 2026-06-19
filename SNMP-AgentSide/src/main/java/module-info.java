e6f888a4aa01ed72010a44d0f447fc0ef29f29cd
Serhat Erdogan <160313394+SerhatErdogann@users.noreply.github.com>
2026-06-19 12:07:21 +0300

    Delete SNMP-AgentSide

--- SNMP-AgentSide/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.snmpagentside {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.snmp4j;
-
-
-    opens org.example.snmpagentside to javafx.fxml;
-    exports org.example.snmpagentside;
-}
